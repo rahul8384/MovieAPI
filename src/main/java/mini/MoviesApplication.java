8bac25b752dee99b454ebc9f3ec1c09c585b2066
rahul8384 <91208315+rahul8384@users.noreply.github.com>
2024-02-01 04:32:20 +0530

    Update MoviesApplication.java

--- src/main/java/mini/MoviesApplication.java
+++ src/main/java/mini/MoviesApplication.java
@@ -2,6 +2,10 @@ package com.minimoviemr.Movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 //@RestController
@@ -11,11 +15,26 @@ public class MoviesApplication {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
-//	@GetMapping("/root") /* this is to change the url or manage the api in the browser
-//	here if we add /root after localhost:8080/root then it'll show us
-//	that we are printing hello world or whatever is in the api end point */
-//
-//	public String apiRoot(){ //this method right here is to create an end point of the api
-//		return "hello world";
+	@Configuration
+	public class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedOrigins("http://localhost:3000")  // Adjust the origin as needed
+							.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+							.allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+							.allowCredentials(true)
+							.maxAge(3600);
+				}
+
+			};
+		}
+	}
+
 //	}
 }
+
